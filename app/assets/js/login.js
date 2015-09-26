272d355fa4ac3365470ca840bc3db136cc512e6d
Sarkie <Sarkie@Gmail.com>
2015-09-26 18:23:42 +0100

    to lower case fix for mobiles and whatever

--- app/assets/js/login.js
+++ app/assets/js/login.js
@@ -1,12 +1,12 @@
 $(function() {
 		$('.login').on('click', function(e) {
 			e.preventDefault();
-			window.location = $('.login-input').val();
+			window.location = $('.login-input').val().toLowerCase();
 		});
 
 		$('.login-input').keyup(function(e) {
 			if(e.keyCode === 13){
-				window.location = $('.login-input').val();
+				window.location = $('.login-input').val().toLowerCase();
 			}
 		});
 });
